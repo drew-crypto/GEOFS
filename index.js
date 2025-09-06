d42ab03c1ac733149d3dddef2864911991c4895d
drew-crypto <andrecheicktraore@gmail.com>
2025-09-07 01:34:50 +0200

    Ajout index.js avec serveur ATC GeoFS

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const PORT = process.env.PORT || 3000;
 io.on("connection", (socket) => {
     console.log("Nouvel utilisateur connecté");
 
+    // Quand un joueur rejoint avec son pseudo
     socket.on("join", (username) => {
         socket.username = username;
         console.log(`${username} a rejoint`);
     });
 
+    // Quand un joueur envoie un message sur un canal
     socket.on("chatMessage", (data) => {
         io.emit("chatMessage", {
             channel: data.channel,
@@ -24,11 +26,13 @@ io.on("connection", (socket) => {
         });
     });
 
+    // Quand un joueur se déconnecte
     socket.on("disconnect", () => {
         console.log(`${socket.username} s'est déconnecté`);
     });
 });
 
+// Démarrage du serveur
 server.listen(PORT, () => {
     console.log(`Serveur Socket.IO lancé sur le port ${PORT}`);
 });
